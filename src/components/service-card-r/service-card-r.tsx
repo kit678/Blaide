61c5fa736e1963acfda8f33b3ce17c4716baa274
kit678 <kit.shukla88@gmail.com>
2024-03-04 15:05:23 -0700

    Added service card component.

---
+++ src/components/service-card-r/service-card-r.tsx
@@ -0,0 +1,25 @@
+import classNames from 'classnames';
+import styles from './service-card-r.module.scss';
+
+export interface ServiceCardRProps {
+    className?: string;
+    heading: string;
+    description: string;
+    img: string;
+}
+
+/**
+ * This component was created using Codux's Default new component template.
+ * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
+ */
+export const ServiceCardR = ({ className, heading, description, img }: ServiceCardRProps) => {
+    return (
+        <div className={classNames(styles.root, className)}>
+            <div className={styles.servicecard}>
+                <h1 />
+                <textarea />
+                <img />
+            </div>
+        </div>
+    );
+};
