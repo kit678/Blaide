c31399597c79c4daac8cb6e394da1e815a38afa8
kit678 <kit.shukla88@gmail.com>
2024-03-04 20:45:04 -0700

    aider: Update image paths to use absolute path from the root of the server.
    # Aider chat conversation:
    
    USER: the image paths are not working, maybe you need to use absolute path ?
    
    ASSISTANT: If the image paths are not working, it's possible that the web server is not serving the assets from the expected directory. To fix this, we can update the image paths to use an absolute path from the root of the server. Here's the diff for the changes to the `Organisations` component:
    
    ```diff
    --- src/components/organisations/organisations.tsx
    +++ src/components/organisations/organisations.tsx
    @@ ... @@
     export const Organisations = ({ className }: OrganisationsProps) => {
         return (
             <div className={classNames(styles.root, className)}>
                 <h2 className={styles.heading}>
                     Our team's expertise in innovation and automation has streamlined business processes, saving millions in man-hours.
                 </h2>
                 <div className={styles.imageCloud}>
    -                <img src="/assets/logos/baruch.svg" alt="Baruch College" />
    -                <img src="/assets/logos/citrix.svg" alt="Citrix" />
    -                <img src="/assets/logos/credit-suisse-1.svg" alt="Credit Suisse" />
    -                <img src="/assets/logos/delta-airlines.svg" alt="Delta Airlines" />
    -                <img src="/assets/logos/ibm.svg" alt="IBM" />
    -                <img src="/assets/logos/illinois-university.svg" alt="University of Illinois" />
    -                <img src="/assets/logos/illinois-university-2.svg" alt="University of Illinois" />
    -                <img src="/assets/logos/manipal.png" alt="Manipal University" />
    -                <img src="/assets/logos/morgan-stanley-new.svg" alt="Morgan Stanley" />
    -                <img src="/assets/logos/stevens-institute-of-technology.svg" alt="Stevens Institute of Technology" />
    -                <img src="/assets/logos/tata-logo.svg" alt="Tata" />
    +                <img src="/src/assets/logos/baruch.svg" alt="Baruch College" />
    +                <img src="/src/assets/logos/citrix.svg" alt="Citrix" />
    +                <img src="/src/assets/logos/credit-suisse-1.svg" alt="Credit Suisse" />
    +                <img src="/src/assets/logos/delta-airlines.svg" alt="Delta Airlines" />
    +                <img src="/src/assets/logos/ibm.svg" alt="IBM" />
    +                <img src="/src/assets/logos/illinois-university.svg" alt="University of Illinois" />
    +                <img src="/src/assets/logos/illinois-university-2.svg" alt="University of Illinois" />
    +                <img src="/src/assets/logos/manipal.png" alt="Manipal University" />
    +                <img src="/src/assets/logos/morgan-stanley-new.svg" alt="Morgan Stanley" />
    +                <img src="/src/assets/logos/stevens-institute-of-technology.svg" alt="Stevens Institute of Technology" />
    +                <img src="/src/assets/logos/tata-logo.svg" alt="Tata" />
                 </div>
             </div>
         );
     };
    ```
    
    This diff prepends `/src` to the existing paths, assuming that the `src` directory is served at the root of the server. If the server is configured differently, you may need to adjust the paths accordingly.

--- src/components/organisations/organisations.tsx
+++ src/components/organisations/organisations.tsx
@@ -16,17 +16,17 @@ export const Organisations = ({ className }: OrganisationsProps) => {
                 Our team's expertise in innovation and automation has streamlined business processes, saving millions in man-hours.
             </h2>
             <div className={styles.imageCloud}>
-                <img src="/assets/logos/baruch.svg" alt="Baruch College" />
-                <img src="/assets/logos/citrix.svg" alt="Citrix" />
-                <img src="/assets/logos/credit-suisse-1.svg" alt="Credit Suisse" />
-                <img src="/assets/logos/delta-airlines.svg" alt="Delta Airlines" />
-                <img src="/assets/logos/ibm.svg" alt="IBM" />
-                <img src="/assets/logos/illinois-university.svg" alt="University of Illinois" />
-                <img src="/assets/logos/illinois-university-2.svg" alt="University of Illinois" />
-                <img src="/assets/logos/manipal.png" alt="Manipal University" />
-                <img src="/assets/logos/morgan-stanley-new.svg" alt="Morgan Stanley" />
-                <img src="/assets/logos/stevens-institute-of-technology.svg" alt="Stevens Institute of Technology" />
-                <img src="/assets/logos/tata-logo.svg" alt="Tata" />
+                <img src="/src/assets/logos/baruch.svg" alt="Baruch College" />
+                <img src="/src/assets/logos/citrix.svg" alt="Citrix" />
+                <img src="/src/assets/logos/credit-suisse-1.svg" alt="Credit Suisse" />
+                <img src="/src/assets/logos/delta-airlines.svg" alt="Delta Airlines" />
+                <img src="/src/assets/logos/ibm.svg" alt="IBM" />
+                <img src="/src/assets/logos/illinois-university.svg" alt="University of Illinois" />
+                <img src="/src/assets/logos/illinois-university-2.svg" alt="University of Illinois" />
+                <img src="/src/assets/logos/manipal.png" alt="Manipal University" />
+                <img src="/src/assets/logos/morgan-stanley-new.svg" alt="Morgan Stanley" />
+                <img src="/src/assets/logos/stevens-institute-of-technology.svg" alt="Stevens Institute of Technology" />
+                <img src="/src/assets/logos/tata-logo.svg" alt="Tata" />
             </div>
         </div>
     );
