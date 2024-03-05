063077a3dbcb74ea91f72c593d4043ed02c4a49a
kit678 <kit.shukla88@gmail.com>
2024-03-04 21:51:43 -0700

    Added CTA component and its styles.

---
+++ src/components/cta/cta.tsx
@@ -0,0 +1,14 @@
+import classNames from 'classnames';
+import styles from './cta.module.scss';
+
+export interface CtaProps {
+    className?: string;
+}
+
+/**
+ * This component was created using Codux's Default new component template.
+ * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
+ */
+export const Cta = ({ className }: CtaProps) => {
+    return <div className={classNames(styles.root, className)}></div>;
+};
