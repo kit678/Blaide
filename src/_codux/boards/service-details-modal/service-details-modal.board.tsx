0751adb400466c3105d93dbcb8fe9cae304c5dab
kit678 <pshukla.contact@gmail.com>
2024-03-12 21:34:26 -0600

    removed service details modal board

--- src/_codux/boards/service-details-modal/service-details-modal.board.tsx
+++
@@ -1,8 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import { ServiceDetailsModal } from '../../../components/service-details-modal/service-details-modal';
-
-export default createBoard({
-    name: 'ServiceDetailsModal',
-    Board: () => <ServiceDetailsModal />,
-    isSnippet: true,
-});
