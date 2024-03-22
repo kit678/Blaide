6380298dec9883de6d6bda9ff48e0184cf1a8b44
kit678 <pshukla.contact@gmail.com>
2024-03-21 23:37:13 -0600

    Add glossy black background to root element.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useMemo } from 'react';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { routes } from './routes';
+import styles from './App.module.scss';
 
 function App() {
     const router = useMemo(() => createBrowserRouter(routes), []);
